029166cf04f0ba3b2926f74268ba27c2e741bb94
Lachlan <lwpearce@gmail.com>
2020-08-10 22:30:23 +1000

    pushed from open-api generator

--- ChronoSheetsClientLibApi/geoFencing.service.ts
+++ ChronoSheetsClientLibApi/geoFencing.service.ts
@@ -17,6 +17,7 @@ import { HttpClient, HttpHeaders, HttpParams,
 import { CustomHttpParameterCodec }                          from '../encoder';
 import { Observable }                                        from 'rxjs';
 
+import { ApiResponseForPaginatedListBasicGeofence } from '../model/models';
 import { ApiResponseForPaginatedListExtendedGeofence } from '../model/models';
 import { ApiResponseGeofence } from '../model/models';
 import { ApiResponseInt32 } from '../model/models';
@@ -342,6 +343,58 @@ export class GeoFencingService {
         return this.httpClient.get<ApiResponseForPaginatedListExtendedGeofence>(`${this.configuration.basePath}/GeoFencing/GetGeofences`,
             {
                 params: queryParameters,
+                responseType: <any>responseType,
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * Gets a list of all geofences in your organisation, including just the name and ID.
+     * @param xChronosheetsAuth The ChronoSheets Auth Token
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public geoFencingGetGeofencesBasicInfo(xChronosheetsAuth: string, observe?: 'body', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml' | 'multipart/form-data'}): Observable<ApiResponseForPaginatedListBasicGeofence>;
+    public geoFencingGetGeofencesBasicInfo(xChronosheetsAuth: string, observe?: 'response', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml' | 'multipart/form-data'}): Observable<HttpResponse<ApiResponseForPaginatedListBasicGeofence>>;
+    public geoFencingGetGeofencesBasicInfo(xChronosheetsAuth: string, observe?: 'events', reportProgress?: boolean, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml' | 'multipart/form-data'}): Observable<HttpEvent<ApiResponseForPaginatedListBasicGeofence>>;
+    public geoFencingGetGeofencesBasicInfo(xChronosheetsAuth: string, observe: any = 'body', reportProgress: boolean = false, options?: {httpHeaderAccept?: 'application/json' | 'text/json' | 'application/xml' | 'text/xml' | 'multipart/form-data'}): Observable<any> {
+        if (xChronosheetsAuth === null || xChronosheetsAuth === undefined) {
+            throw new Error('Required parameter xChronosheetsAuth was null or undefined when calling geoFencingGetGeofencesBasicInfo.');
+        }
+
+        let headers = this.defaultHeaders;
+        if (xChronosheetsAuth !== undefined && xChronosheetsAuth !== null) {
+            headers = headers.set('x-chronosheets-auth', String(xChronosheetsAuth));
+        }
+
+        let httpHeaderAcceptSelected: string | undefined = options && options.httpHeaderAccept;
+        if (httpHeaderAcceptSelected === undefined) {
+            // to determine the Accept header
+            const httpHeaderAccepts: string[] = [
+                'application/json',
+                'text/json',
+                'application/xml',
+                'text/xml',
+                'multipart/form-data'
+            ];
+            httpHeaderAcceptSelected = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        }
+        if (httpHeaderAcceptSelected !== undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+
+        let responseType: 'text' | 'json' = 'json';
+        if(httpHeaderAcceptSelected && httpHeaderAcceptSelected.startsWith('text')) {
+            responseType = 'text';
+        }
+
+        return this.httpClient.get<ApiResponseForPaginatedListBasicGeofence>(`${this.configuration.basePath}/GeoFencing/GetGeofencesBasicInfo`,
+            {
                 responseType: <any>responseType,
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
